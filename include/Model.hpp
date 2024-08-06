81a7f03c83cc1564e1bf41aceff7ff42d45c1b7f
Kevin Tu <kevin.xander.tu@gmail.com>
2024-08-05 22:30:03 -0500

    Remove unused size functions

--- include/Model.hpp
+++ include/Model.hpp
@@ -15,12 +15,13 @@ private:
 	std::vector<std::vector<glm::ivec3>> faces;
 public:
 	Model(const char* filename);
-	size_t numVertices() const { return vertices.size(); }
-	size_t numFaces() const { return faces.size(); }
+
 	glm::vec3 getVertex(unsigned int i) const { return vertices.at(i); }
 	glm::vec3 getVertex(glm::ivec3 vec) const { return vertices.at(vec[VERTEX]); }
+
 	glm::vec3 getTextureUV(unsigned int i) const { return textureUVs.at(i); }
 	glm::vec3 getTextureUV(glm::ivec3 vec) const { return textureUVs.at(vec[TEXTURE]); }
+
 	std::vector<glm::ivec3> getFace(int i) const { return faces.at(i); }
 	const std::vector<std::vector<glm::ivec3>>& getFaces() const { return faces; }
 };
