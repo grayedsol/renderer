a6c2d613da31d33a208a64d5949ef9f3bc21eac3
Kevin Tu <kevin.xander.tu@gmail.com>
2024-08-13 17:46:10 -0500

    Clean up Triangle namespace

--- include/Triangle.hpp
+++ include/Triangle.hpp
@@ -1,23 +1,24 @@
 #pragma once
 #include "tgaimage.hpp"
-#include "glm/ext/vector_float3.hpp"
-#include "glm/ext/vector_int2.hpp"
+#include "glm/ext/matrix_int2x2.hpp"
+#include "glm/ext/matrix_float3x2.hpp"
 #include "glm/ext/matrix_float3x3.hpp"
 #include "glm/ext/matrix_int3x2.hpp"
-#include "glm/ext/matrix_int2x2.hpp"
 
 namespace Triangle {
     using ivec2 = glm::ivec2;
+    using vec2 = glm::vec2;
     using vec3 = glm::vec3;
     using mat3 = glm::mat3;
     using imat2x2 = glm::imat2x2;
     using imat3x2 = glm::imat3x2;
+    using mat3x2 = glm::mat3x2;
 
-    inline int edge(const glm::ivec2 a, const glm::ivec2 b, const glm::ivec2 c) {
+    inline float edge(const glm::vec2 a, const glm::vec2 b, const glm::vec2 c) {
         return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
     }
 
-    inline int edge(const glm::imat3x2 m) {
+    inline float edge(const glm::mat3x2 m) {
         return (m[1].x - m[0].x) * (m[2].y - m[0].y) - (m[1].y - m[0].y) * (m[2].x - m[0].x);
     }
 
@@ -29,25 +30,21 @@ namespace Triangle {
         return (m[1].x - m[0].x) * (m[2].y - m[0].y) - (m[1].y - m[0].y) * (m[2].x - m[0].x);
     }
 
-    imat2x2 boundingBox(const imat3x2 tri);
+    imat2x2 boundingBox(const mat3x2 tri);
 
-    imat2x2 boundingBox(int screenWidth, int screenHeight, const imat3x2 tri);
+    imat2x2 boundingBox(int screenWidth, int screenHeight, const mat3x2 tri);
 
     imat2x2 boundingBox(const mat3 tri);
 
     imat2x2 boundingBox(int screenWidth, int screenHeight, const mat3 tri);
 
+    vec3 barycentric(const mat3x2 tri, const vec2 point);
+
     vec3 barycentric(const mat3 tri, const vec3 point);
 
     void draw(const imat3x2 tri, TGAImage& image, const TGAColor& color);
 
-    void fill2d(imat3x2 tri, TGAImage& image, const TGAColor& color);
+    void fill2(mat3x2 tri, TGAImage& image, const TGAColor& color);
 
-    void fill(mat3 tri, TGAImage& image, const TGAColor& color, float* zBuffer);
-
-    void fillTexture(mat3 tri, mat3 uv, TGAImage& image, const TGAImage* texture, float intensity, float* zBuffer);
-
-    void fillTextureGouraud(mat3 tri, mat3 uv, mat3 norms, TGAImage& image, const TGAImage* texture, const vec3 lightDirection, float* zBuffer);
-
-    void fillGradient(mat3 tri, TGAImage& image);
+    void fill3(mat3 tri, TGAImage& image, const TGAColor& color, float* zBuffer);
 }
--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -3,7 +3,7 @@
 #include <utility>
 #include "glm/ext/matrix_transform.hpp"
 
-glm::imat2x2 Triangle::boundingBox(const imat3x2 tri) {
+glm::imat2x2 Triangle::boundingBox(const mat3x2 tri) {
 	return imat2x2 {
         {
             std::min(tri[0].x, std::min(tri[1].x, tri[2].x)), 
@@ -16,16 +16,16 @@ glm::imat2x2 Triangle::boundingBox(const imat3x2 tri) {
     };
 }
 
-glm::imat2x2 Triangle::boundingBox(int screenWidth, int screenHeight, const imat3x2 tri) {
+glm::imat2x2 Triangle::boundingBox(int screenWidth, int screenHeight, const mat3x2 tri) {
     using std::min, std::max;
     return {
         {
-            max(0, min(screenWidth - 1, min(tri[0].x, min(tri[1].x, tri[2].x)))),
-            max(0, min(screenHeight - 1, min(tri[0].y, min(tri[1].y, tri[2].y))))
+            max(0, min(screenWidth - 1, (int)min(tri[0].x, min(tri[1].x, tri[2].x)))),
+            max(0, min(screenHeight - 1, (int)min(tri[0].y, min(tri[1].y, tri[2].y))))
         },
         {
-            max(0, min(screenWidth - 1, max(tri[0].x, max(tri[1].x, tri[2].x)))),
-            max(0, min(screenWidth - 1, max(tri[0].y, max(tri[1].y, tri[2].y))))
+            max(0, min(screenWidth - 1, (int)max(tri[0].x, max(tri[1].x, tri[2].x)))),
+            max(0, min(screenWidth - 1, (int)max(tri[0].y, max(tri[1].y, tri[2].y))))
         }
     };
 }
@@ -57,6 +57,14 @@ glm::imat2x2 Triangle::boundingBox(int screenWidth, int screenHeight, const mat3
     };
 }
 
+glm::vec3 Triangle::barycentric(const mat3x2 tri, const vec2 point) {
+    return vec3 {
+        edge(tri[1], tri[2], point),
+        edge(tri[2], tri[0], point),
+        edge(tri[0], tri[1], point)   
+    };
+}
+
 glm::vec3 Triangle::barycentric(const mat3 tri, const vec3 point) {
     return vec3 {
         edge(tri[1], tri[2], point),
@@ -71,21 +79,22 @@ void Triangle::draw(const imat3x2 tri, TGAImage &image, const TGAColor &color) {
     drawLine(tri[2], tri[0], image, color);
 }
 
-void Triangle::fill2d(imat3x2 tri, TGAImage &image, const TGAColor &color) {
+void Triangle::fill2(mat3x2 tri, TGAImage &image, const TGAColor &color) {
     if (edge(tri) < 0) { std::swap(tri[1], tri[2]); }
     imat2x2 bounds = boundingBox(image.get_width(), image.get_height(), tri);
 
     for (int x = bounds[0].x; x <= bounds[1].x; x++) {
         for (int y = bounds[0].y; y <= bounds[1].y; y++) {
+            vec3 bary = barycentric(tri, vec2{x,y});
             const ivec2 pt{x,y};
-            if ((edge(tri[0], tri[1], pt) | edge(tri[1], tri[2], pt) | edge(tri[2], tri[0], pt)) > 0) {
+            if ((bary[0] >= 0 && bary[1] >= 0 && bary[2] >=0)) {
                 image.set(x, y, color);
             }
         }
     }
 }
 
-void Triangle::fill(mat3 tri, TGAImage &image, const TGAColor &color, float *zBuffer) {
+void Triangle::fill3(mat3 tri, TGAImage &image, const TGAColor &color, float *zBuffer) {
     float twoArea = edge(tri);
     if (twoArea < 0) {
         std::swap(tri[1], tri[2]);
@@ -103,88 +112,6 @@ void Triangle::fill(mat3 tri, TGAImage &image, const TGAColor &color, float *zBu
                     zBuffer[x+(y*width)] = z;
                     image.set(x, y, color);
                 }
-            }
-        }
-    }
-}
-
-void Triangle::fillTexture(mat3 tri, mat3 uv, TGAImage &image, const TGAImage *texture, float intensity, float *zBuffer) {
-    float twoArea = edge(tri);
-    if (twoArea < 0) {
-        std::swap(tri[1], tri[2]);
-        std::swap(uv[1], uv[2]);
-        twoArea *= -1.f;
-    }
-    imat2x2 bounds = boundingBox(image.get_width(), image.get_height(), tri);
-
-    const int width = image.get_width();
-    const int texWidth = texture->get_width();
-    const int texHeight = texture->get_height();
-    for (int x = bounds[0].x; x <= bounds[1].x; x++) {
-        for (int y = bounds[0].y; y <= bounds[1].y; y++) {
-            vec3 bary = Triangle::barycentric(tri, vec3{x,y,0.f}) / twoArea;
-            if ((bary[0] >= 0 && bary[1] >= 0 && bary[2] >=0)) {
-                float z = glm::dot(bary, vec3{tri[0].z, tri[1].z, tri[2].z});
-                if (zBuffer[x+(y*width)] < z) {
-                    zBuffer[x+(y*width)] = z;
-                    vec3 texPoint = uv * bary;
-                    TGAColor color = texture->get(int(texWidth * texPoint.x), texHeight - int(texHeight * texPoint.y)) * intensity;
-                    image.set(x, y, color);
-                }
-            }
-        }
-    }
-}
-
-void Triangle::fillTextureGouraud(mat3 tri, mat3 uv, mat3 norms, TGAImage &image, const TGAImage *texture, const vec3 lightDirection, float *zBuffer) {
-    float twoArea = edge(tri);
-    if (twoArea < 0) {
-        std::swap(tri[1], tri[2]);
-        std::swap(norms[1], norms[2]);
-        std::swap(uv[1], uv[2]);
-        twoArea *= -1.f;
-    }
-    imat2x2 bounds = boundingBox(image.get_width(), image.get_height(), tri);
-
-    const int width = image.get_width();
-    const int texWidth = texture->get_width();
-    const int texHeight = texture->get_height();
-    for (int x = bounds[0].x; x <= bounds[1].x; x++) {
-        for (int y = bounds[0].y; y <= bounds[1].y; y++) {
-            vec3 bary = Triangle::barycentric(tri, vec3{x,y,0.f}) / twoArea;
-            if ((bary[0] >= 0 && bary[1] >= 0 && bary[2] >=0)) {
-                float z = glm::dot(bary, vec3{tri[0].z, tri[1].z, tri[2].z});
-                if (zBuffer[x+(y*width)] < z) {
-                    zBuffer[x+(y*width)] = z;
-                    float intensity = -glm::dot(norms * bary, lightDirection);
-                    if (intensity <= .0f) { intensity = .0f; }
-                    vec3 texPoint = uv * bary;
-                    TGAColor color = texture->get(int(texWidth * texPoint.x), texHeight - int(texHeight * texPoint.y)) * intensity;
-                    image.set(x, y, color);
-                }
-            }
-        }
-    }
-}
-
-void Triangle::fillGradient(mat3 tri, TGAImage &image) {
-    float twoArea = edge(tri);
-    if (twoArea < 0) {
-        std::swap(tri[1], tri[2]);
-        twoArea *= -1.f;
-    }
-    imat2x2 bounds = boundingBox(image.get_width(), image.get_height(), tri);
-
-    for (int x = bounds[0].x; x <= bounds[1].x; x++) {
-        for (int y = bounds[0].y; y <= bounds[1].y; y++) {
-            vec3 bary = Triangle::barycentric(tri, vec3{x,y,0.f}) / twoArea;
-            if ((bary[0] >= 0 && bary[1] >= 0 && bary[2] >=0)) {
-                image.set(x, y, TGAColor{
-                    (unsigned char)(bary[0] * 255),
-                    (unsigned char)(bary[1] * 255),
-                    (unsigned char)(bary[2] * 255),
-                    255
-                });
             }
         }
     }
