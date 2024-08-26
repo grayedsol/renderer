fd1d4b2223b0801f29f0f47427a8a863920cada5
Kevin Tu <kevin.xander.tu@gmail.com>
2024-08-26 15:51:39 -0500

    Remove interpolate, move fragCoord.w calculation

--- include/Rasterize.hpp
+++ include/Rasterize.hpp
@@ -10,15 +10,6 @@ namespace Rasterize {
 	using imat2x2 = glm::imat2x2;
 	using mat3x4 = glm::mat3x4;
 
-	float interpolate(mat3x4 tri, int index, vec3 barycentric) {
-		assert(index < 4);
-		float foo = 0.f;
-		for (int i = 0; i < 3; i++) {
-			foo += tri[i][index] * barycentric[i];
-		}
-		return foo;
-	}
-
 	template<typename FShader>
 	void triangle(mat3x4 tri, mat3 world, mat3 uv, mat3 tbns[3], const FShader fragShader, OutImage& image) {
 		static_assert(std::is_base_of<FragmentShader, FShader>());
@@ -38,15 +29,16 @@ namespace Rasterize {
 		FragmentShaderData fragData;
 		fragData.uv = uv;
 		fragData.copyTBNs(tbns);
+		
 		for (int x = bounds[0].x; x <= bounds[1].x; x++) {
 			for (int y = bounds[0].y; y <= bounds[1].y; y++) {
 				vec4 fragCoord{ x, y, 0.f, 0.f };
 				vec3 bary = Triangle::barycentric(tri, fragCoord) / twoArea;
 				if ((bary[0] >= 0 && bary[1] >= 0 && bary[2] >= 0)) {
 					fragCoord.z = glm::dot(bary, vec3{ tri[0].z, tri[1].z, tri[2].z });
-					fragCoord.w = glm::dot(bary, vec3{ tri[0].w, tri[1].w, tri[2].w });
 					if (zBuffer[x + (y * width)] < fragCoord.z) {
 						zBuffer[x + (y * width)] = fragCoord.z;
+						fragCoord.w = glm::dot(bary, vec3{ tri[0].w, tri[1].w, tri[2].w });
 						fragData.baryCoord = Triangle::perspectiveBarycentric(tri, fragCoord, bary);
 						fragData.fragCoord = world * fragData.baryCoord;
 						image.set(x, y, fragShader(fragData));
